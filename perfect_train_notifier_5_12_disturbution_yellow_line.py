1ddec1ae824d8ce2009d6ad871ef8f1e87aa6aa9
Neepa23 <upadhyayniparth@gmail.com>
2020-01-08 11:01:27 +0000

    Yellow_ message

---
+++ perfect_train_notifier_5_12_disturbution_yellow_line.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+import boto3
+
+
+
+# ------------------- E-mail list ------------------------
+toAddress = ['nipa23pandya@gmail.com','neepa.pandya@cydar.co.uk']
+# --------------------------------------------------------
+page =requests.get("https://www.thameslinkrailway.com/travel-information/plan-your-journey/service-updates")
+soup =BeautifulSoup(page.content, 'html.parser')
+#print(soup)
+#period = tonight.find(class_ ="period-name").get_text()
+#seven_day = soup.find(id ="seven-day-forecast")
+seven_day =soup.find_all(class_ ="c-disruptions-item__title c-combined-messages__link c-combined-messages__link--disruption")
+print(seven_day)
+
+
+
+
+for item in seven_day:
+    conn = smtplib.SMTP ( 'smtp.gmail.com' , 587 )  # smtp address and port
+    conn.ehlo ()  # call this to start the connection
+    conn.starttls ()  # starts tls encryption. When we send our password it will be encrypted.
+    conn.login ( 'trial0error0@gmail.com' , '0banana0' )
+    # conn.sendmail ( 'trial0error0@gmail.com' , toAddress ,'Subject: weather Alert!\n\nAttention!\n\nAware about waether!\n\nCareful!:\nweather notifier' )
+    # conn.quit()
+    report = ""
+    print ( 'Sent notificaton e-mails for the following recipients:\n' )
+    for i in range ( len ( toAddress ) ):
+           print ( toAddress[i] )
+           report = item.find_all("div", {"class": "o-layout__left@lg o-grid__item@lg"})[0].get_text()
+           print( item.find_all("div", {"class": "o-layout__left@lg o-grid__item@lg"})[0].get_text())
+           print("###")
+
+    conn.sendmail ( 'trial0error0@gmail.com' , toAddress ,f'Subject: Train Alert!\n\nAttention!\n\nAware about Train Disturbance!\n\nCareful!:\nTrain notifier\n{report}' )
+    conn.quit()
+
+#forecast_items = seven_day.find_all(class_ = "tombstone-container")
+# items = seven_day.find_all(class_ = "o-layout@lg o-grid@lg c-combined-messages__title")
+# tonight = items[0]
+# print(items)
+# print(tonight)
+# #print(forecast_items)
+# period = tonight.find(class_ ="o-layout__left@lg o-grid__item@lg").get_text()
+# # # period = forecast_items.find_all(class_ ="o-layout__left@lg o-grid__item@lg").get_text()
+# # # #period = soup.find(".c-disruptions-item__title c-combined-messages__link c-combined-messages__link--disruption .o-layout__left@lg o-grid__item@lg").get_text()
+# print(period)
+
